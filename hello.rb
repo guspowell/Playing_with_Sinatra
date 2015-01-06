7631ccc5b2cf9a6406bb365921f5efb3b15b5c60
guspowell <augustuspowell@me.com>
2015-01-06 11:27:22 +0000

    created views file and index.erb

--- hello.rb
+++ hello.rb
@@ -1,13 +1,9 @@
 require 'sinatra'
 
 get '/' do
-	"<div style='border: 3px dashed red'>
-		<img src= 'http://bit.ly/1eze8aE'>
-	</div>"
+	erb :index
 end
 
 get '/secret' do
   'This is a secret page'
 end
-
-gus
