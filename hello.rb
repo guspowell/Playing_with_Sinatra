4175988d5fa7a554651856693a147bed5d97dc28
India Dearlove <india_dearlove@yahoo.co.uk>
2015-01-06 09:28:31 +0000

    first commit

---
+++ hello.rb
@@ -0,0 +1,7 @@
+require 'sinatra'
+
+get '/' do
+  "<div style= 'border: 3px dashed red'>
+    <img src ='http://i2.mirror.co.uk/incoming/article99763.ece/alternates/s2197/a-bengal-cat-935179217.jpg'>
+  </div>"
+end
