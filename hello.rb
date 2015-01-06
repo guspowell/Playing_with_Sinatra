b966c5705cd4e13dbe0f2b57d0beec8a5b7dacea
India Dearlove <india_dearlove@yahoo.co.uk>
2015-01-06 09:32:16 +0000

    secret page

--- hello.rb
+++ hello.rb
@@ -5,3 +5,7 @@ get '/' do
     <img src ='http://i2.mirror.co.uk/incoming/article99763.ece/alternates/s2197/a-bengal-cat-935179217.jpg'>
   </div>"
 end
+
+get '/secret' do
+  'This is a secret page'
+end
